600b61d82f0441d1d44a1445b9d9da7d261687df
Divankar Ramhit <ramhit828@gmail.com>
2023-05-07 00:24:00 +0400

    Fixed Build Errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
 		extend: {
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-				'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+				'gradient-conic':'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+				'main-bg': "url('/assets/images/light-bg.jpg')",
 			},
 			colors: {
 				primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
