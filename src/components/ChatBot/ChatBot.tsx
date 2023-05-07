a88d7065b3a025c92707951ade1ba1e1cf68d317
Divankar Ramhit <ramhit828@gmail.com>
2023-05-07 08:29:15 +0400

    Buttons

--- src/components/ChatBot/ChatBot.tsx
+++ src/components/ChatBot/ChatBot.tsx
@@ -48,9 +48,9 @@ const ChatBot = () => {
 
             {
                 predicted == true ?
-                <div>
-                    <button onClick={resetChatbot}>Make another prediction</button>
-                    <button onClick={sendDetailedReport}>Send detailed report</button>
+                <div className="flex my-7">
+                    <button onClick={resetChatbot} className="text-black mx-2 font-medium rounded-lg transition-all ease-in-out bg-[#FFA500] hover:bg-[#FFA500]/[.6] hover:rounded-3xl text-sm px-5 py-2.5 flex justify-center items-center">Make another prediction</button>
+                    <button onClick={sendDetailedReport} className="text-black mx-2 font-medium rounded-lg transition-all ease-in-out bg-teal-300 hover:bg-teal-300/[.6] hover:rounded-3xl text-sm px-5 py-2.5 flex justify-center items-center">Email Me a detailed Report</button>
                 </div>
                 :
                 <></>
