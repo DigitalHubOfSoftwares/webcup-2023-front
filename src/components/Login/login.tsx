326a14e7a9a9e5808706fb720876cb3bea68eeb5
Divankar Ramhit <ramhit828@gmail.com>
2023-05-07 07:53:00 +0400

    Login And Register

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -8,18 +8,19 @@ interface Props {
     setEmail: any;
     password: any;
     setPassword: any;
+	IsLoading: any;
 }
 
-const Login : React.FC<Props> = ({ onSubmit, email, setEmail, password, setPassword }) => {
+const Login : React.FC<Props> = ({ onSubmit, email, setEmail, password, setPassword, IsLoading }) => {
 
 	return(
-		<section className="bg-gray-50 dark:bg-gray-900">
+		<section className="bg-dark-bg bg-no-repeat bg-cover bg-center dark:bg-dark-bg">
 			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 				<Link href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-					<Image className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" width={0} height={0} alt="logo"/>
-					{ process.env.NEXT_PUBLIC_APP_NAME }    
+				<Image src={process.env.NEXT_PUBLIC_PREFIX_URL + '/assets/images/idi-logo.jpg'} width={140} height={200} className="mr-3 h-6 sm:h-9 rounded" alt="Flowbite Logo" />
+					{/* { process.env.NEXT_PUBLIC_APP_NAME }     */}
 				</Link>
-				<div className="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+				<div className="w-full bg-white rounded-lg shadow-2xl dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800/[.6] dark:backdrop-blur-sm dark:border-gray-700">
 					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
 							Sign in to your account
@@ -27,26 +28,27 @@ const Login : React.FC<Props> = ({ onSubmit, email, setEmail, password, setPassw
 						<form className="space-y-4 md:space-y-6" onSubmit = {onSubmit}>
 							<div>
 								<label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-								<input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" onChange={(e) => setEmail(e.target.value)} value={email} required/>
+								<input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#BF13E3] focus:border-[#BF13E3] block w-full p-2.5 dark:bg-gray-700/[.15] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#BF13E3] dark:focus:border-[#BF13E3]" placeholder="name@company.com" onChange={(e) => setEmail(e.target.value)} value={email} required/>
 							</div>
 							<div>
 								<label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-								<input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+								<input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#BF13E3] focus:border-[#BF13E3] block w-full p-2.5 dark:bg-gray-700/[.15] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#BF13E3] dark:focus:border-[#BF13E3]" value={password} onChange={(e) => setPassword(e.target.value)} required/>
 							</div>
-							<div className="flex items-center justify-between">
-								<div className="flex items-start">
-									<div className="flex items-center h-5">
-										<input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"/>
-									</div>
-									<div className="ml-3 text-sm">
-										<label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-									</div>
-								</div>
-								<Link href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</Link>
-							</div>
-							<button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+							
+							<button type="submit" className="w-full text-white font-medium rounded-lg transition-all ease-in-out bg-[#81007f] hover:bg-[#BF13E3]/[.6] hover:rounded-3xl text-sm px-5 py-2.5 flex justify-center items-center">
+							{IsLoading == false ?
+								"Log In" 
+								:
+								(
+								<svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+									<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+								</svg>
+								)
+							}
+							</button>
 							<p className="text-sm font-light text-gray-500 dark:text-gray-400">
-								Don’t have an account yet? <Link href="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+								Don’t have an account yet? <Link href="/register" className="font-medium text-[#81007f] hover:underline dark:text-[#BF13E3]">Sign up</Link>
 							</p>
 						</form>
 					</div>
